0173b278ad930ae3b0fec4289d4d416ecba74f52
Danish Iqbal <danish112iqbal@gmail.com>
2025-10-04 12:54:42 +0530

    "fix-navigation"

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
@@ -12,7 +12,10 @@ const Header = () => {
           <Nav className="ms-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to="/technology">Technology</Nav.Link>
-            <Nav.Link as={Link} to="/investment">Investment</Nav.Link>
+            <NavDropdown title="Investment" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/investment/blogs">Blogs</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/caplin-point">Caplin Point</NavDropdown.Item>
+            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
