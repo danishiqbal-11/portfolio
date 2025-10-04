5b663b9a2add008f4d18458f6f30b7ead6a32346
Danish Iqbal <danish112iqbal@gmail.com>
2025-10-04 13:32:57 +0530

    "feat-add-dsa-blogs"

---
+++ src/data/dsaBlogs.js
@@ -0,0 +1,12 @@
+import Arrays from '../components/technology/dsa/Arrays';
+
+export const dsaBlogs = [
+  {
+    id: 'arrays',
+    title: 'Introduction to Arrays',
+    description: 'An array is a collection of items of same data type stored at contiguous memory locations.',
+    path: '/technology/dsa/blogs/arrays',
+    component: Arrays,
+  },
+  // Add more DSA blog posts here
+];
