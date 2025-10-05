d518ab71b1ea2bb049c2649a94a7eef02f9f2718
Danish Iqbal <danish112iqbal@gmail.com>
2025-10-05 18:14:09 +0530

    feat: add resources section to investment page

--- src/components/Investment.js
+++ src/components/Investment.js
@@ -78,6 +78,28 @@ const Investment = () => {
             </Card>
           </Col>
         </Row>
+
+        <hr className="my-5" />
+
+        <h3 className="text-center my-4">Resources</h3>
+        <Row>
+          <Col md={4} className="mb-4">
+            <Card as="a" href="https://www.youtube.com/watch?v=oj7l_0Moxs8&list=PL6WVSL3D1Ek9ukPuR_tDCRRQiaOulxx1N" target="_blank" className="text-decoration-none h-100 shadow-sm">
+              <Card.Body className="d-flex flex-column">
+                <div className="text-center">
+                  <i className="bi bi-film fs-1 text-danger"></i>
+                  <Card.Title className="mt-3">Behavioural Finance</Card.Title>
+                </div>
+                <Card.Text className="text-center">
+                  A playlist of videos on behavioural finance by Parag Parikh at GIM.
+                </Card.Text>
+                <div className="text-center mt-auto">
+                  <span className="btn btn-primary">Watch on YouTube</span>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </Container>
     </section>
   );
