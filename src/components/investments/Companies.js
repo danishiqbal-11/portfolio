8b8befe10c1e10b5e59b9e0ed94c9f683eaee119
Danish Iqbal <danish112iqbal@gmail.com>
2025-10-05 18:46:39 +0530

    fix: import alert component in companies page

--- src/components/investments/Companies.js
+++ src/components/investments/Companies.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col, Card, Alert } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Companies = () => {
